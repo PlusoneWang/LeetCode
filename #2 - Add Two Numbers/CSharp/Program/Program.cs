1367120c46253f9ef528d8c6afc9d67b1be4dc89
Plusone <plusonewang82@gmail.com>
2019-03-08 17:31:08 +0800

    CSharp: #2 Add Two Numbers

---
+++ #2 - Add Two Numbers/CSharp/Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static ListNode AddTwoNumbers(ListNode l1, ListNode l2, int? landing)
+        {
+            var val = (l1?.val ?? 0) + (l2?.val ?? 0) + (landing ?? 0);
+            var currentNode = new ListNode(val % 10);
+            int? nextLanding = null;
+            if (val >= 10)
+            {
+                nextLanding = val / 10;
+            }
+
+            if (l1?.next != null || l2?.next != null)
+            {
+                currentNode.next = AddTwoNumbers(l1?.next, l2?.next, nextLanding);
+                return currentNode;
+            }
+
+            if (nextLanding.HasValue)
+                currentNode.next = new ListNode(nextLanding.Value);
+            return currentNode;
+        }
+    }
+}
